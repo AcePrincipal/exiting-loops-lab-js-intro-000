5a329a7c4898cb935fc606f60a3812f0de2ed5aa
Alexander Petrov <acepetrov96@gmail.com>
2020-09-30 23:58:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,11 @@ function keepGoing(array, changeValue, skipValue) {
 }
 
 function findBy(array, findFn) {
-  findFn(array);
+  for(let i = 0; i < array.length; i++) {
+    if (array[i] === findFn){
+      return array[i]; 
+    }
+  }
   
   return null;
 }
